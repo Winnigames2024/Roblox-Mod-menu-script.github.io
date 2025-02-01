b9bad47c3abdcd661d3186029803b8d4139b5aa6
Winnigames2024 <familycontroll2024@gmail.com>
2025-02-01 09:35:41 +0500

    Update modmenu.lua

--- modmenu.lua
+++ modmenu.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/Winnigames2024/Roblox-Mod-menu-script.github.io/blob/main/modmenuscript.lua"))()
+loadstring(game:HttpGet("https://github.com/Winnigames2024/Roblox-Mod-menu-script.github.io/releases/download/Script/modmenu.txt"))(https://t.me/winnigames2024)
